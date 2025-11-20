b4d802e460590b6f11e1fc9cc76538bf33427ec2
Alex80-m <a.mois80@yandex.ru>
2025-11-20 20:00:22 +0300

    Add files via upload

---
+++ HelloWord.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    // Программа начинается с вызова main()
+    public static void main(String args) {
+        // Выводит «Hello, World!» в окно терминала
+        System.out.println("Hello, World");
+    }
+}
